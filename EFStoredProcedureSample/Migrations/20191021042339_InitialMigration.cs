68fff9cf90409829fbcc58bb9ab345a3f71b608e
Niels Swimberghe <nielsswimberghe@gmail.com>
2019-10-21 00:29:33 -0400

    Add project files.

---
+++ EFStoredProcedureSample/Migrations/20191021042339_InitialMigration.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EFStoredProcedureSample.Migrations
+{
+    public partial class InitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Pets",
+                columns: table => new
+                {
+                    PetId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(nullable: true),
+                    Type = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Pets", x => x.PetId);
+                });
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Pets");
+        }
+    }
+}
---
+++ EFStoredProcedureSample/Migrations/20191021042834_CreatePetStoredProcedures.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EFStoredProcedureSample.Migrations
+{
+    public partial class CreatePetStoredProcedures : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+                CREATE PROCEDURE GetPetsByType @Type varchar(50)
+                AS   
+                    SET NOCOUNT ON;
+                    SELECT [PetId], [Name], [Type]  
+                    FROM Pets
+                    WHERE [Type] = @Type;
+                GO  
+            ");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+                DROP PROCEDURE GetPetsByType;  
+                GO  
+            ");
+        }
+    }
+}
